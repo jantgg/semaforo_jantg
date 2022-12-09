81f10c21a5652d700387ce7a7a0101256057cec2
jantgg <smosh360@hotmail.com>
2022-12-09 20:28:13 +0000

    semaforo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import '/workspace/react-hello/src/styles/index.css';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [color, setColor] = useState("")
+	const colors = ['red', 'yellow', 'green']
+	
+	const roll = (arr, n) => {
+		const randomN = [];
+		while(randomN.length < n){
+		  const randomIndex = Math.floor(Math.random()*arr.length);
+		  randomN.push(arr[randomIndex]);
+		  arr.splice(randomIndex, 1);
+		}
+		return randomN;}
+	  
+	  let swapper = (roll(colors, 1)); 
+	  
+	
+
+  return (
+	<div>
+		<div className="row vh-100 d-flex justify-content-center pt-5">
+			<div id="father" className="col-4 mt-5 d-flex align-items-start flex-column black">
+				<button id="light1" className={"w-50 h-25 mx-auto mt-5 circle " + (color == "red" ? color : "gray")}  onClick={()=>setColor("red")}></button>
+				<button id="light2" className={"w-50 h-25 mx-auto mt-5 circle " + (color == "yellow" ? color : "gray")}  onClick={()=>setColor("yellow")}></button>
+				<button id="light3" className={"w-50 h-25 mx-auto mt-5 circle " + (color == "green" ? color : "gray")}  onClick={()=>setColor("green")}></button>
+			</div>
+			<div id="div2" className="col-2 mt-5 d-flex align-items-center">
+			<button id="button" className="rounded" onClick={()=>setColor(swapper)}><strong>Press me</strong></button>
+			</div>
 		</div>
-	);
+		
+	</div>
+  );
 };
 
 export default Home;
